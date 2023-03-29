52a4187c4e043c418b44f85404298f17be6112ad
Joao Maciel <joao.maciel.amb@gmail.com>
2023-03-29 08:35:37 -0300

    audio_links_ok

--- main.js
+++ main.js
@@ -24,12 +24,18 @@ document.addEventListener("DOMContentLoaded", function () {
       section: "1 --> Introdução à Exposição 🌊",
       panels: [
         {
-          panel: 1,
+          panel: "1 · Seja bem-vindo",
           title: "Boas-vindas",
-          url: "https://example.com/panel1.mp3",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2001.mp3?raw=true",
         },
-        { panel: 2, url: "https://example.com/panel2.mp3" },
-        { panel: 3, url: "https://example.com/panel3.mp3" },
+        {
+          panel: "2 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2002.mp3?raw=true",
+        },
+        {
+          panel: "3 · Importância dos Oceanos",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2003.mp3?raw=true",
+        },
       ],
     },
 
@@ -38,12 +44,30 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       section: "2 --> Conceitos Físicos 🔬",
       panels: [
-        { panel: 4, url: "https://example.com/panel4.mp3" },
-        { panel: 5, url: "https://example.com/panel5.mp3" },
-        { panel: 6, url: "https://example.com/panel6.mp3" },
-        { panel: 7, url: "https://example.com/panel7.mp3" },
-        { panel: 8, url: "https://example.com/panel8.mp3" },
-        { panel: 9, url: "https://example.com/panel9.mp3" },
+        {
+          panel: "4 · Formação dos Oceanos",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2004.mp3?raw=true",
+        },
+        {
+          panel: "5 · As Praias São Diferentes?",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2006.mp3?raw=true",
+        },
+        {
+          panel: "6 · Imagens de Sedimentos",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2006.mp3?raw=true",
+        },
+        {
+          panel: "7 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2007.mp3?raw=true",
+        },
+        {
+          panel: "8 · Praias Refletivas",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2008.mp3?raw=true",
+        },
+        {
+          panel: "9 · Imagem de Hermitão",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2009.mp3?raw=true",
+        },
       ],
     },
 
@@ -52,12 +76,30 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       section: "3 --> Conceitos Humanos 🌍",
       panels: [
-        { panel: 10, url: "https://example.com/panel10.mp3" },
-        { panel: 11, url: "https://example.com/panel11.mp3" },
-        { panel: 12, url: "https://example.com/panel12.mp3" },
-        { panel: 13, url: "https://example.com/panel13.mp3" },
-        { panel: 14, url: "https://example.com/panel14.mp3" },
-        { panel: 15, url: "https://example.com/panel15.mp3" },
+        {
+          panel: "10 · Povos Costeiros",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2010.mp3?raw=true",
+        },
+        {
+          panel: "11 · Sambaquis e Sambaquieiros",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2011.mp3?raw=true",
+        },
+        {
+          panel: "12 · Ocanos no seu Dia a Dia",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2012.mp3?raw=true",
+        },
+        {
+          panel: "13 · Linha do Tempo",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2013.mp3?raw=true",
+        },
+        {
+          panel: "14 · Reflexão sobre Consumismo (a)",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2014.mp3?raw=true",
+        },
+        {
+          panel: "15 · Reflexão sobre Consumismo (b)",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2015.mp3?raw=true",
+        },
       ],
     },
 
@@ -66,48 +108,122 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       section: "4 --> Como o Lixo Chega no Mar? 🗑️",
       panels: [
-        { panel: 16, url: "https://example.com/panel16.mp3" },
-        { panel: 17, url: "https://example.com/panel17.mp3" },
-        { panel: 18, url: "https://example.com/panel18.mp3" },
-        { panel: 19, url: "https://example.com/panel19.mp3" },
+        {
+          panel: "16 · Como o Lixo Chega no Mar?",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2016.mp3?raw=true",
+        },
+        {
+          panel: "17 · Ilhas de Lixo",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2017.mp3?raw=true",
+        },
+        {
+          panel: "18 · Degradação do Plástico",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2018.mp3?raw=true",
+        },
+        {
+          panel: "19 · Tipos de Microplástico",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2019.mp3?raw=true",
+        },
       ],
     },
 
     {
       section: "5 --> O Impacto na Fauna Marinha 🐠🐬",
       panels: [
-        { panel: 20, url: "https://example.com/panel20.mp3" },
-        { panel: 21, url: "https://example.com/panel21.mp3" },
-        { panel: 22, url: "https://example.com/panel22.mp3" },
-        { panel: 23, url: "https://example.com/panel23.mp3" },
+        {
+          panel: "20 · Microplástico na Cadeia Alimentar",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2020.mp3?raw=true",
+        },
+        {
+          panel: "21 · Bioacumulação",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2021.mp3?raw=true",
+        },
+        {
+          panel: "22 · Impacto na Fauna Marinha",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2022.mp3?raw=true",
+        },
+        {
+          panel: "23 · Redes Fantasmas",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2023.mp3?raw=true",
+        },
       ],
     },
 
     {
       section: "6 --> Chegamos ao fundo? 🌊🔍",
-      panels: [{ panel: "24 - 28", url: "https://example.com/panel24.mp3" }],
+      panels: [
+        {
+          panel: "24 e 25 · Imagem de Resíduos Flutuando",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2024%20e%2025.mp3?raw=true",
+        },
+        {
+          panel: "26 a 28 · Chegamos ao Fundo?",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2026%2027%20e%2028.mp3?raw=true",
+        },
+      ],
     },
 
     {
       section: "7 --> A Solução Está Aqui! 💡",
       panels: [
-        { panel: 29, url: "https://example.com/panel20.mp3" },
-        { panel: 30, url: "https://example.com/panel21.mp3" },
-        { panel: 31, url: "https://example.com/panel22.mp3" },
-        { panel: 32, url: "https://example.com/panel23.mp3" },
+        {
+          panel: "29 · A Solução está Aqui",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2029.mp3?raw=true",
+        },
+        {
+          panel: "30 · O que é Sustentabilidade?",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2030.mp3?raw=true",
+        },
+        {
+          panel: "31 · Antigo eu x Novo eu",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2031.mp3?raw=true",
+        },
+        {
+          panel: "32 · Mais vida, menos lixo!",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2032.mp3?raw=true",
+        },
       ],
     },
 
     {
       section: "8 --> Conheça nossas Ações Sustentáveis ♻️🌱",
       panels: [
-        { panel: 33, url: "https://example.com/panel20.mp3" },
-        { panel: 34, url: "https://example.com/panel21.mp3" },
-        { panel: 35, url: "https://example.com/panel22.mp3" },
-        { panel: 36, url: "https://example.com/panel23.mp3" },
-        { panel: 37, url: "https://example.com/panel22.mp3" },
-        { panel: 38, url: "https://example.com/panel22.mp3" },
-        { panel: 39, url: "https://example.com/panel22.mp3" },
+        {
+          panel: "33 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2033.mp3?raw=true",
+        },
+        {
+          panel: "34 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2034.mp3?raw=true",
+        },
+        {
+          panel: "35 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2035.mp3?raw=true",
+        },
+        {
+          panel: "36 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2036.mp3?raw=true",
+        },
+        {
+          panel: "37 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2037.mp3?raw=true",
+        },
+        {
+          panel: "38 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2038.mp3?raw=true",
+        },
+        {
+          panel: "39 · Expressão Artística",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2039.mp3?raw=true",
+        },
+        {
+          panel: "40 a 42 · Qual é a sua Praia?",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/Mar%20Adentro%20Painel%2040%2041%20e%2042.mp3?raw=true",
+        },
+        {
+          panel: "43 · Encerramento",
+          url: "https://github.com/ambmajociel/mar_adentro/blob/main/audioguias/mar%20adentro%20encerramento.mp3?raw=true",
+        },
       ],
     },
   ];
